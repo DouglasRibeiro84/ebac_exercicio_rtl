b7bacf840ec574c4c2e9a9f42c95ff2d0f31c901
doouglas <douglasribeiro084@gmail.com>
2025-03-26 09:19:59 -0300

    Finalizado o exercicio de testar 2 comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("Deve inserir 2 comentarios", () => {
+    render(<PostComment />);
+    fireEvent.change(screen.getByTestId("textarea-teste"), {
+      target: {
+        value: "Inserindo o primeiro comentario",
+      },
     });
+    fireEvent.click(screen.getByTestId("btn-comentario-teste"));
+
+    fireEvent.change(screen.getByTestId("textarea-teste"), {
+      target: {
+        value: "Inserindo o segundo comentario",
+      },
+    });
+    fireEvent.click(screen.getByTestId("btn-comentario-teste"));
+
+    expect(screen.getAllByTestId("lista-teste")).toHaveLength(2);
+  });
 });
